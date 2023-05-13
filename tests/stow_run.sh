822eedace877e65ed45375150531e0f838c38267
Bartek Kucypera <bartek.kucypera@gazeta.pl>
2023-05-13 22:51:38 +0200

    super_commit

--- tests/stow_run.sh
+++ tests/stow_run.sh
@@ -1,7 +1,7 @@
-rm usr/src/minix/servers/pm/*.c
-rm usr/src/minix/servers/pm/*.h
-rm usr/src/minix/servers/pm/Makefile
+rm /usr/src/minix/servers/pm/*.c
+rm /usr/src/minix/servers/pm/*.h
+rm /usr/src/minix/servers/pm/Makefile
 
-rm usr/src/minix/include/minix/*.h
+rm /usr/src/minix/include/minix/*.h
 
 stow --dir=/root/MINIX/fake_mount --target=/ .
