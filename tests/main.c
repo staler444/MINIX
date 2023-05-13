3afca0ee51361142705529d85b01c18fd12628e2
Bartek Kucypera <bartek.kucypera@gazeta.pl>
2023-05-13 21:42:07 +0200

    super_commit

--- tests/main.c
+++ tests/main.c
@@ -1,11 +1,14 @@
 #include <lib.h>
 #include <minix/rs.h>
 #include <stdio.h>
+#include <minix/callnr.h>
 
 int main()
 {
         message m;
         endpoint_t pm_ep;
         minix_rs_lookup("pm", &pm_ep);
+        printf("%d", IS_PM_CALL(PM_TRANSFER_MONEY));
+
         printf("%d", _syscall(pm_ep, PM_TRANSFER_MONEY, &m));
 }
