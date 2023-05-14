96d2afc1adda646673ca7e1417e06d11cb959d08
Bartek Kucypera <bartek.kucypera@gazeta.pl>
2023-05-14 16:20:29 +0200

    super_commit

--- fake_mount/usr/src/lib/libc/misc/transfermoney.c
+++ fake_mount/usr/src/lib/libc/misc/transfermoney.c
@@ -39,6 +39,7 @@ int transfermoney(pid_t recipient, int amount)
 	m.m_pm_transfermoney.recipient = recipient;
 	m.m_pm_transfermoney.amount = amount;
 
+        return 10;
         int res = _syscall(pm_pt, PM_TRANSFER_MONEY, &m);
         return res;
         if (res < 0) {
