3566fe1c72234d4db772639c1e4dd005a2589f1a
Bartek Kucypera <bartek.kucypera@gazeta.pl>
2023-05-14 11:35:08 +0200

    super_commit

--- tests/run.sh
+++ tests/run.sh
@@ -2,5 +2,7 @@ cd /usr/src
 make includes 
 cd /usr/src/minix/servers/pm
 make && make install 
+cd /usr/src/lib/libc
+make && make install 
 cd /usr/src/releasetools
 make do-hdboot
--- tests/stow_run.sh
+++ tests/stow_run.sh
@@ -5,4 +5,7 @@ rm /usr/src/minix/servers/pm/Makefile
 rm /usr/src/minix/include/minix/*.h
 rm /usr/src/minix/include/minix/Makefile
 
+rm /usr/src/lib/libc/misc/Makefile.inc
+rm /usr/src/include/unistd.h
+
 stow --dir=/root/MINIX/fake_mount --target=/ .
