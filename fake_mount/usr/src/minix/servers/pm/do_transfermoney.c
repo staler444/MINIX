04ac03a1c435be8eae65d1b59ed73ea6fa6a38a4
Bartek Kucypera <bartek.kucypera@gazeta.pl>
2023-05-14 16:01:21 +0200

    super_commit

--- fake_mount/usr/src/minix/servers/pm/do_transfermoney.c
+++ fake_mount/usr/src/minix/servers/pm/do_transfermoney.c
@@ -14,13 +14,11 @@ int dirty_money_check(struct mproc* a, struct mproc* b)
 }
 
 int do_transfermoney(void) {
-	if (m_in.m_type != PM_TM_MSG_TRANSFER)
-		return PM_TM_PANIC;
-
 	pid_t dest_pid = m_in.m_pm_transfermoney.recipient;
 	struct mproc* dest_ptr = find_proc(dest_pid);
 	if (dest_ptr == NULL)
 		return PM_TM_NON_EXISTING_PID;
+
 	if (dirty_money_check(mp, dest_ptr) == -1)
 		return PM_TM_DIRTY_MONEY;
 
